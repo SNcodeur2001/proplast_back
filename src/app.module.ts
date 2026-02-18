18b9fc7d963e07ca7e66d4893a54ad1d92600211
SNcondeur2001 <mapathendiaye542@gmail.com>
2026-02-18 16:39:22 +0000

    setup complète

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ClientsModule } from './clients/clients.module';
+import { ImportsModule } from './imports/imports.module';
+import { StatsModule } from './stats/stats.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, ClientsModule, ImportsModule, StatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Validation globale DTO
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+
+  // CORS
+  app.enableCors();
+
+  // Logger (Nest déjà inclus)
+  await app.listen(3000);
+  console.log(`Server running on http://localhost:3000`);
 }
 bootstrap();
